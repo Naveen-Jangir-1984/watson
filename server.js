0fa35de586d025acfcc421901b7c9e6d7f5309b8
NJ <monu.bits@gmail.com>
2025-03-21 11:24:54 +0530

    baseline code

---
+++ server.js
@@ -0,0 +1,383 @@
+const CryptoJS = require("crypto-js");
+const express = require("express");
+const multer = require('multer');
+const path = require("path");
+const https = require("https");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json({ limit: "50mb" })); // Increase JSON payload limit
+app.use(express.urlencoded({ limit: "50mb", extended: true })); // Increase URL-encoded payload limit
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+app.use("/videos", express.static(path.join(__dirname, "public/videos")));
+
+require("dotenv").config();
+const port = process.env.REACT_APP_PORT;
+const secretKey = process.env.REACT_APP_SECRET_KEY;
+
+const resource = '/api/watson';
+const database = './database/watson.json';
+
+const options = {
+  key: fs.readFileSync("./certificate.key"),
+  cert: fs.readFileSync("./certificate.crt"),
+};
+
+const encryptData = (data) => {
+  return CryptoJS.AES.encrypt(JSON.stringify(data), secretKey).toString();
+};
+
+const decryptData = (encryptedData) => {
+  const bytes = CryptoJS.AES.decrypt(encryptedData, secretKey);
+  return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+};
+
+// ---------- WATSON ----------
+
+app.get("/events", (req, res) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+
+  setInterval(() => {
+    fs.readFile(database, (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const db = JSON.parse(data);
+      const feed = {
+        enquiries: db.enquiries,
+        posts: db.posts,
+        headlines: db.headlines,
+        events: db.events,
+        posters: db.posters
+      }
+      res.write(`data: ${encryptData(feed)}\n\n`);
+    });
+  }, 10000);
+});
+
+app.get(`${resource}/data`, (req, res) => {
+  fs.readFile(database, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const db = JSON.parse(data);
+    const remoteAddress = req.socket.remoteAddress.split('::ffff:')[1];
+    if(!db.visitors.includes(remoteAddress)) {
+      db.visitors = [...db.visitors, remoteAddress];
+      fs.writeFile(database, JSON.stringify(db, null, 2), (err) => {
+        if (err) res.send({ result: 'failed' });
+        // res.send({ result: 'success' });
+      });      
+    }
+    res.send(encryptData(db));
+  });
+});
+
+app.post(`${resource}/addFeedback`, (req, res) => {
+  fs.readFile(database, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    db = JSON.parse(data);
+    const feedback = req.body.feedback;
+    db.posts = [decryptData(feedback), ...db.posts];
+    fs.writeFile(database, JSON.stringify(db, null, 2), (err) => {
+      if (err) res.send(encryptData({ result: 'failed' }));
+      res.send(encryptData({ result: 'success' }));
+    });
+  });
+});
+
+app.post(`${resource}/deleteFeedback`, (req, res) => {
+  fs.readFile(database, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    db = JSON.parse(data);
+    const id = req.body.id;
+    db.posts = db.posts.filter(post => post.id !== id);
+    fs.writeFile(database, JSON.stringify(db, null, 2), (err) => {
+      if (err) res.send({ result: 'failed' });
+      res.send({ result: 'success' });
+    });
+  });
+});
+
+app.post(`${resource}/addEvent`, (req, res) => {
+  fs.readFile(database, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    db = JSON.parse(data);
+    const event = req.body.event;
+    db.events = [decryptData(event), ...db.events];
+    fs.writeFile(database, JSON.stringify(db, null, 2), (err) => {
+      if (err) res.send(encryptData({ result: 'failed' }));
+      res.send(encryptData({ result: 'success' }));
+    });
+  });
+});
+
+app.post(`${resource}/deleteEvent`, (req, res) => {
+  fs.readFile(database, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    db = JSON.parse(data);
+    const id = req.body.id;
+    db.events = db.events.filter(event => event.id !== id);
+    fs.writeFile(database, JSON.stringify(db, null, 2), (err) => {
+      if (err) res.send({ result: 'failed' });
+      res.send({ result: 'success' });
+    });
+  });
+});
+
+app.post(`${resource}/addHeadline`, (req, res) => {
+  fs.readFile(database, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    db = JSON.parse(data);
+    const headline = req.body.headline;
+    db.headlines = [decryptData(headline), ...db.headlines];
+    fs.writeFile(database, JSON.stringify(db, null, 2), (err) => {
+      if (err) res.send(encryptData({ result: 'failed' }));
+      res.send(encryptData({ result: 'success' }));
+    });
+  });
+});
+
+app.post(`${resource}/deleteHeadline`, (req, res) => {
+  fs.readFile(database, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    db = JSON.parse(data);
+    const id = req.body.id;
+    db.headlines = db.headlines.filter(headline => headline.id !== id);
+    fs.writeFile(database, JSON.stringify(db, null, 2), (err) => {
+      if (err) res.send({ result: 'failed' });
+      res.send({ result: 'success' });
+    });
+  });
+});
+
+app.post(`${resource}/addEnquiry`, (req, res) => {
+  fs.readFile(database, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    db = JSON.parse(data);
+    const enquiry = req.body.enquiry;
+    db.enquiries = [decryptData(enquiry), ...db.enquiries];
+    fs.writeFile(database, JSON.stringify(db, null, 2), (err) => {
+      if (err) res.send(encryptData({ result: 'failed' }));
+      res.send(encryptData({ result: 'success' }));
+    });
+  });
+});
+
+app.post(`${resource}/resetEnquiry`, (req, res) => {
+  fs.readFile(database, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    db = JSON.parse(data);
+    db.enquiries.map(enquiry => {
+      enquiry.status = 'read'
+      return enquiry;
+    });
+    fs.writeFile(database, JSON.stringify(db, null, 2), (err) => {
+      if (err) res.send(encryptData({ result: 'failed' }));
+      res.send(encryptData({ result: 'success' }));
+    });
+  });
+});
+
+app.post(`${resource}/deleteEnquiry`, (req, res) => {
+  fs.readFile(database, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    db = JSON.parse(data);
+    const id = decryptData(req.body.id);
+    db.enquiries = db.enquiries.filter(enquiry => enquiry.id !== id);
+    fs.writeFile(database, JSON.stringify(db, null, 2), (err) => {
+      if (err) res.send(encryptData({ result: 'failed' }));
+      res.send(encryptData({ result: 'success' }));
+    });
+  });
+});
+
+app.post(`${resource}/updateTimeTable`, (req, res) => {
+  fs.readFile(database, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    db = JSON.parse(data);
+    const timetable = decryptData(req.body.timetable);
+    db.timetables.forEach(tt => {
+      if(tt.id === timetable.id) {
+        tt.standard = timetable.standard;
+        tt.hours = timetable.hours;
+        tt.start = timetable.start;
+        tt.startHour = timetable.startHour;
+        tt.end = timetable.end;
+        tt.endHour = timetable.endHour;
+        tt.subjects = timetable.subjects
+      }
+      return timetable;
+    });
+    fs.writeFile(database, JSON.stringify(db, null, 2), (err) => {
+      if (err) res.send(encryptData({ result: 'failed' }));
+      res.send(encryptData({ result: 'success' }));
+    });
+  });
+});
+
+app.post(`${resource}/addTimeTable`, (req, res) => {
+  fs.readFile(database, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    db = JSON.parse(data);
+    const index = decryptData(req.body.index);
+    const timetable = decryptData(req.body.timetable);
+    db.timetables = [...db.timetables.slice(0, index), timetable, ...db.timetables.slice(index)];
+    fs.writeFile(database, JSON.stringify(db, null, 2), (err) => {
+      if (err) res.send(encryptData({ result: 'failed' }));
+      res.send(encryptData({ result: 'success' }));
+    });
+  });
+});
+
+app.post(`${resource}/deleteTimeTable`, (req, res) => {
+  fs.readFile(database, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    db = JSON.parse(data);
+    const id = decryptData(req.body.id);
+    db.timetables = db.timetables.filter(timetable => timetable.id !== id);
+    fs.writeFile(database, JSON.stringify(db, null, 2), (err) => {
+      if (err) res.send(encryptData({ result: 'failed' }));
+      res.send(encryptData({ result: 'success' }));
+    });
+  });
+});
+
+// Configure Multer
+const storage = multer.diskStorage({
+  destination: "public/images/Posters",
+  filename: (req, file, cb) => {
+    const folderPath = path.join(__dirname, "public/images/Posters");
+
+    fs.readdir(folderPath, (err, files) => {
+      if (err) {
+        console.error("Error reading directory:", err);
+        return cb(err);
+      }
+
+      const fileIds = files.filter(file => {
+        if(file.startsWith('image')) {
+          return file.split('.')[0].substring(5);
+        }
+      });
+      let randomId = Math.floor(Math.random() * 1000) + 1;
+      while(fileIds.includes(randomId)) {
+        randomId = Math.floor(Math.random() * 1000) + 1;
+      };
+      const extension = path.extname(file.originalname);
+      const fileName = `image${randomId}${extension}`;
+
+      cb(null, fileName);
+    });
+  },
+});
+const upload = multer({ storage, limits: { fileSize: 50 * 1024 * 1024 } });
+
+app.post(`${resource}/addPoster`, upload.single('file'), (req, res) => {
+  fs.readFile(database, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    
+    db = JSON.parse(data);
+    const filename = req.file.filename;
+    const id = Number(filename.split('.')[0].substring(5));
+    const newPoster = {
+      id: id,
+      logo: `/images/Posters/${filename}`,
+      isSelected: false
+    };
+    db.posters = [newPoster, ...db.posters];
+    fs.writeFile(database, JSON.stringify(db, null, 2), (err) => {
+      if (err) res.send(encryptData({ result: 'failed' }));
+      res.send(encryptData({ result: 'success' }));
+    });
+  });
+});
+
+app.post(`${resource}/deletePoster`, (req, res) => {
+  const folderPath = path.join(__dirname, "public/images/Posters");
+  fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      console.error("Error reading directory:", err);
+      return res.status(500).json({ error: "Failed to read directory" });
+    }
+    const id = decryptData(req.body.id);
+    const fileToDelete = files.find((file) => file.startsWith(`image${id}`));
+    const filePath = path.join(folderPath, fileToDelete);
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.error("Error deleting file:", err);
+        return res.status(500).json({ error: "Failed to delete file" });
+      }
+    });
+  });
+  fs.readFile(database, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const id = decryptData(req.body.id);
+    db = JSON.parse(data);
+    db.posters = db.posters.filter(poster => poster.id !== id);
+    fs.writeFile(database, JSON.stringify(db, null, 2), (err) => {
+      if (err) res.send(encryptData({ result: 'failed' }));
+      res.send(encryptData({ result: 'success' }));
+    });
+  });
+});
+
+// ---------- WATSON ----------
+
+// app.listen(port, () => {
+//   console.log(`listening at http://localhost:${port}`);
+// });
+
+https.createServer(options, app).listen(port, () => {
+  console.log(`listening at https://localhost:${port}`);
+});
